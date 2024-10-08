0c03571a3778bec98b8c2ca255d3f0aa18b4cce4
bijoy <bsbijoy2050@gmail.com>
2024-10-08 08:43:20 -0400

    Fix vllm script

--- vllm.sh
+++ vllm.sh
@@ -10,10 +10,3 @@ CUDA_VISIBLE_DEVICES=0 vllm serve \
   /shared_archive/common/models--meta-llama--Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659/ \
   --dtype auto \
   --api-key token-abc123
-
-# 4. Run SWE-agent on SWE-bench Lite and generate patches
-python run.py --model_name gpt4 \
-  --instance_filter marshmallow-code__marshmallow-1359
-# python run.py --model_name llama-3.1-8b \
-#   --per_instance_cost_limit 0.00 \
-#   --config_file ./config/default.yaml
