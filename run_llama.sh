d12a8df8672e0883f8b042166fc90fa8cbb3cea2
bijoy <bsbijoy2050@gmail.com>
2024-10-10 22:49:05 -0400

    Added model path

--- run_llama.sh
+++ run_llama.sh
@@ -4,6 +4,8 @@ conda activate swe_agent_env_v2
 
 python -m pip install --upgrade pip && pip install --editable .
 
+docker pull sweagent/swe-agent:latest
+
 python run.py --model_name llama-3.1-8b \
   --instance_filter marshmallow-code__marshmallow-1359
 # python run.py --model_name llama-3.1-8b \
