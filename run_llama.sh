ea2e2d3aa9f63ca5a33e6f5c447413f193e12d46
bijoy <bsbijoy2050@gmail.com>
2024-10-10 22:46:28 -0400

    Added model path

--- run_llama.sh
+++ run_llama.sh
@@ -1,6 +1,6 @@
 # Run SWE-agent on SWE-bench Lite and generate patches
-conda create -n swe_agent_env python=3.9.7
-conda activate swe_agent_env
+conda create -n swe_agent_env_v2 python=3.9.7
+conda activate swe_agent_env_v2
 
 python run.py --model_name llama-3.1-8b \
   --instance_filter marshmallow-code__marshmallow-1359
