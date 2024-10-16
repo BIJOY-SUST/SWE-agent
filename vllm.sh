8558b9a854ba2b99b1c544382824da1053a77352
bijoy <bsbijoy2050@gmail.com>
2024-10-16 13:46:47 -0400

    Modified code

--- vllm.sh
+++ vllm.sh
@@ -14,7 +14,7 @@ conda activate vllm_env
 pip list
 echo "vllm installed successfully"
 # 3. Start the vllm server with Llama 3.1 8B Instruct
-CUDA_VISIBLE_DEVICES=0,1 vllm serve \
+CUDA_VISIBLE_DEVICES=2,3 vllm serve \
   /shared_archive/common/models--meta-llama--Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659/ \
   --dtype auto \
   --api-key token-abc123 \
