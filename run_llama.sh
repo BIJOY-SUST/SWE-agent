f226a9b754df0a212ce2480dadffe8f698c010fc
bijoy <bsbijoy2050@gmail.com>
2024-10-29 12:02:34 -0400

    Modified code

--- run_llama.sh
+++ run_llama.sh
@@ -9,5 +9,7 @@
 # python run.py --model_name llama-3.1-8b \
 #   --instance_filter marshmallow-code__marshmallow-1359
 python run.py --model_name llama-3.1-8b \
-  --per_instance_cost_limit 0.00 \
-  --config_file ./config/default.yaml
+  --instance_filter astropy__astropy-12907
+# python run.py --model_name llama-3.1-8b \
+#   --per_instance_cost_limit 0.00 \
+#   --config_file ./config/default.yaml
