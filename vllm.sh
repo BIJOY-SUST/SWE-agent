3edf5db8ec3dfcf833a1d5d2c866f9e248a6e150
Biddut Sarker Bijoy <bsbijoy2050@gmail.com>
2024-11-08 17:27:20 -0500

    modify vllm.sh

--- vllm.sh
+++ vllm.sh
@@ -1,22 +1,22 @@
 # 1. Create and activate a new conda environment
-conda create -n vllm_env2 python=3.9.7
-echo "vllm environment created successfully"
-conda activate vllm_env2
-echo "vllm environment activated successfully"
+# conda create -n vllm_env2 python=3.9.7
+# echo "vllm environment created successfully"
+# conda activate vllm_env2
+# echo "vllm environment activated successfully"
 # 2. Install vllm using pip
-pip install vllm
-pip install --upgrade jinja2
-pip install --upgrade openai
+#pip install vllm
+#pip install --upgrade jinja2
+#pip install --upgrade openai
 
-conda deactivate
-echo "vllm environment deactivated successfully"
-conda activate vllm_env2
-pip list
-echo "vllm installed successfully"
+#conda deactivate
+#echo "vllm environment deactivated successfully"
+#conda activate vllm_env2
+#pip list
+#echo "vllm installed successfully"
 # 3. Start the vllm server with Llama 3.1 8B Instruct
-CUDA_VISIBLE_DEVICES=1 vllm serve \
+CUDA_VISIBLE_DEVICES=0 vllm serve \
   /shared_archive/common/models--meta-llama--Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659/ \
   --dtype auto \
   --api-key token-abc123 \
   --gpu-memory-utilization 0.9 \
-  --port 8001
+  --port 8000
